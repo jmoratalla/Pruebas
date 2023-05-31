a128f576f4bc1e7f20aa7b70d7f5cbe488b95d90
Javier Moratalla <33413895+jmoratalla@users.noreply.github.com>
2023-05-31 12:21:51 +0200

    Add files via upload

---
+++ prueba02/src/main/java/prueba2/prueba02/Prueba02Application.java
@@ -0,0 +1,13 @@
+package prueba2.prueba02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba02Application.class, args);
+	}
+
+}
---
+++ prueba02/src/test/java/prueba2/prueba02/Prueba02ApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba2.prueba02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
